15890cd602db40c70a2c3c95ff19be8da2b0428b
Rodrigo Lima <roamaral98@hotmail.com>
2026-07-09 17:20:17 -0300

    Add files via upload

---
+++ fadeOutPageAnim.js
@@ -0,0 +1,27 @@
+const desktopPage = document.querySelector(".desktop-page");
+
+function fadingOutPage() {
+  desktopPage.addEventListener("fadingOutProcess", function () {
+    desktopPage.style.animation = "fadeOutPage 0.5s ease-in-out forwards";
+    console.log("fading Out Now");
+  });
+}
+
+fadingOutPage();
+
+function triggerFadingOut(element) {
+  const fadingOutEvent = new Event("fadingOutProcess");
+  desktopPage.dispatchEvent(fadingOutEvent);
+
+  const destination = element.dataset.href;
+
+  setTimeout(() => {
+    window.location.href = destination;
+  }, 500);
+}
+
+window.addEventListener("pageshow", function () {
+  desktopPage.style.animation = "none";
+  void desktopPage.offsetWidth;
+  desktopPage.style.animation = "fadeInPage 0.5s ease-in-out forwards";
+});
